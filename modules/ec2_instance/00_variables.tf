f7b2503971a5a45d2434cc34b1769865e1518ff5
ElsieMarion <elcymao@gmail.com>
2024-01-09 18:00:23 +0300

    removed availability zones variables

--- modules/ec2_instance/00_variables.tf
+++ modules/ec2_instance/00_variables.tf
@@ -1,15 +1,17 @@
 variable "ami" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID for EC2 instance"
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-}
-
-variable "count" {
-  description = "Number of EC2 instances to create"
+  description = "Instance type for  EC2 instance"
 }
 
 variable "subnet_id" {
-  description = "Subnet ID for the EC2 instance"
+  description = "subnet ID for EC2 instance"
+  type        = map(string)
 }
+
+# variable "availability_zones" {
+#   description = "AZs used"
+#   type        = list(string)
+# }
