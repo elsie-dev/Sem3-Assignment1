69fee2785605cfbe4d8df3e1906820201814721b
ElsieMarion <elcymao@gmail.com>
2024-01-09 18:01:44 +0300

    variables defined

--- modules/ec2_instance/02_main.tf
+++ modules/ec2_instance/02_main.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "ec2_instance" {
+  for_each = var.availability_zones
+
   ami           = var.ami
   instance_type = var.instance_type
-  count         = var.count
-  subnet_id     = var.subnet_id
+  subnet_id     = var.subnet_id[each.key]
 
   # Add other necessary configurations
 }
-
-
